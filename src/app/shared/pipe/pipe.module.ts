f74beb21f3109e2f5787519142d78e3f9e75a3ec
Dixit <mca.dixit03@gmail.com>
2023-07-26 15:08:02 +0530

    Auto-generated commit [ci skip]

--- src/app/shared/pipe/pipe.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { ArraySortPipe, GroupByPipe, NiceDateFormatPipe, NiceTimePipe } from './common.pipe';
-
-@NgModule({
-  imports: [],
-  declarations: [NiceTimePipe, GroupByPipe, ArraySortPipe, NiceDateFormatPipe],
-  exports: [NiceTimePipe, GroupByPipe, ArraySortPipe, NiceDateFormatPipe]
-})
-export class PipeModule { }
--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  apiUrl: 'http://localhost:3000/v1/',
-  uploadsUrl: 'http://localhost:3000/uploads/'
-};
